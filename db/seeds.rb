a541292dbd6321ccf4188cdbf263c14c5d41feaf
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-11-24 12:43:08 +0100

    Create Seeds, routes controller and view of show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+Booking.destroy_all
+Activity.destroy_all
+User.destroy_all
+
+puts 'Creating an user'
+
+user1 = User.create(nickname: 'Rebecca Buendia', email: 'rebeccabuendia@gmail.com', password:'123456')
+user2 = User.create(nickname: 'Pilar Ternera', email: 'pilarternera@gmail.com', password:"654321")
+user3 = User.create(nickname: 'Amaranta', email: 'amaranta@gmail.com', password:"1234567")
+
+puts 'User created'
+
+
+puts 'Creating activities'
+
+kundalini_yoga = Activity.create(name: 'Kundalini Yoga', description: 'Kundalini Yoga is a magical science that uses sound, mantra, energy healing, exercises and meditations to release trauma from the energetic body, which surrounds the physical body. It is this field, known as the aura, that holds wounds. When those wounds are healed, radiance can occur. Radiance is the magnetic frequency that draws in beauty, love, and light. Attracting abundance into your life starts in the subtle (energetic) body–not the mind.', address: 'Lübbener Str. 9, 10997 Berlin', start_at: DateTime.new(2021,12,5,4,3), end_at: DateTime.new(2020,12,5,5,3), user: user1)
+
+self_defence_training = Activity.create(name: 'My Self Defence', description: 'My Self Defense will help you to gain set of awareness, assertiveness, verbal confrontation skills, safety strategies, and physical techniques that will enable you to escape, resist, and survive violent attacks. It will provides psychological awareness and verbal skills, not just physical training.', address: 'Bredowstraße 18, 10551 Dortmund', start_at: DateTime.new(2020,12,8,4,0), end_at: DateTime.new(2021,12,5,6,0), user: user2)
+
+lotus_dance = Activity.create(name: 'Lotus Dance', description: "Lotus Dance is a dancing class based on specific body's movement that will help to find a healthier equilibrium, both physiologically and emotionally. It improves hormonal and nervous-system regulation, and it helps people connect with their bodies, which expands their capacity to counter dysregulation and to trust themselves. We truly believe that movements also make people more resilient physically and emotionally.", address: 'Gerichtstraße 23, 13347 Berlin', start_at: DateTime.new(2020,12,1,4,3), end_at: DateTime.new(2021,12,5,5,3), user: user3)
+
+puts 'Activities created'
