519cb079808e3d0fb956da4c1f66c6a41f04c778
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-24 13:31:39 +0100

    migration image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_155438) do
+ActiveRecord::Schema.define(version: 2020_11_24_122347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_155438) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
