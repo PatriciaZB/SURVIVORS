f5fd142adcdd96493cd25e99bef5e7b52be0eed6
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-26 12:08:00 +0100

    seeds finally

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_121317) do
+ActiveRecord::Schema.define(version: 2020_11_26_100726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,11 @@ ActiveRecord::Schema.define(version: 2020_11_25_121317) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
-    t.float "latitude"
-    t.float "longitude"
     t.string "category"
     t.string "presence"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "Altimage"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
