3f0fdc7bb5699de2aa5632d28ff3371c22a2a5ee
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-30 12:51:19 +0100

    index questions almost ready

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -12,7 +12,7 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      const popup = new mapboxgl.Popup({font-size: '2px'}).setHTML(marker.infoWindow);
 
       new mapboxgl.Marker({color: '#CE796B'})
         .setLngLat([ marker.lng, marker.lat ])
