569638e860111266730bc75b691e3c8037fe9e09
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-11-30 10:55:55 +0100

    rollback to the correct migration

--- db/migrate/20201130095306_add_title_to_questions.rb
+++ db/migrate/20201130095306_add_title_to_questions.rb
@@ -1,5 +1,5 @@
 class AddTitleToQuestions < ActiveRecord::Migration[6.0]
   def change
-    add_column :questions, :title, :text
+    add_column :questions, :title, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_094512) do
+ActiveRecord::Schema.define(version: 2020_11_30_095306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_094512) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "title"
+    t.string "title"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
