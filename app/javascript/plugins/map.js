0059a10bfd232eef04abd0badfe1d93b96459dc4
Bruna Portugal <brunabportugal@gmail.com>
2020-11-30 11:56:25 +0100

    mapbox windows fixed + hovering effects

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -11,16 +11,20 @@ const initMapbox = () => {
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
+
+    const mapMarkers = [];
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
-      new mapboxgl.Marker({color: '#CE796B'})
+      const newMarker = new mapboxgl.Marker({color: '#CE796B'})
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(map);
+      mapMarkers.push(newMarker);
     });
 
     fitMapToMarkers(map, markers);
+    openInfoWindow(mapMarkers);
   }
 };
 
@@ -29,6 +33,22 @@ const fitMapToMarkers = (map, markers) => {
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
+
+const openInfoWindow = (markers) => {
+  // Select all cards
+  const cards = document.querySelectorAll('.card-ws');
+  cards.forEach((card, index) => {
+    // Put a microphone on each card listening for a mouseenter event
+    card.addEventListener('mouseenter', () => {
+      // Here we trigger the display of the corresponding marker infoWindow with the "togglePopup" function provided by mapbox-gl
+      markers[index].togglePopup();
+    });
+    // We also put a microphone listening for a mouseleave event to close the modal when user doesn't hover the card anymore
+    card.addEventListener('mouseleave', () => {
+      markers[index].togglePopup();
+    });
+  });
+}
 
 
 
