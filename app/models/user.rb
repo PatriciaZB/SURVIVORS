428189ed9579680d225f1289cbd3fc52e9bbbc5f
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-30 18:14:55 +0100

    problems fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -12,4 +13,5 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
 end
