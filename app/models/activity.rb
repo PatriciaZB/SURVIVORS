2cc98056640b04b5aaf3768cf76026b677565c4a
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-02-06 18:59:42 +0100

    Add comments to explain the groups and put the costants on the top of the model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,17 +1,22 @@
 class Activity < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  has_many :bookings, dependent: :destroy
-
-  belongs_to :user
-
-  CATEGORIES = ["Yoga", "Art", "Dance", "Painting", "Self defense", "Group therapy", "Talk", "Cooking", "Workshop", "Theater", "Movement", "Sports", "Protest", "Music", "Meditation", "Other"]
+  # Constants
+  CATEGORIES = ["Yoga", "Art", "Dance", "Painting", "Self defense", "Group therapy",
+                "Talk", "Cooking", "Workshop", "Theater", "Movement", "Sports",
+                "Protest", "Music", "Meditation", "Other"]
   PRESENCE = ["In-presence", "Online"]
 
+  # ActiveRecord Relations
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  belongs_to :user
+  # Active Storage
+  has_one_attached :image
+
+  # Gem-specific stuff
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  has_one_attached :image
-
+  # Validations
   validates :name, presence: true
   validates :description, presence: true
   validates :address, presence: true
